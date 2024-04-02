a9e0fe66c36ed0a841ef57b26096003a11bce231
remolinmg <castroroyfrancis05@gmail.com>
2024-04-02 09:26:58 +0800

    Update home.php

--- login_user/home.php
+++ login_user/home.php
@@ -400,7 +400,8 @@ if ($result->num_rows > 0) {
                               $stmt->bind_param("si", $pending, $order_id);
                               $pending = "cancelled";
                               $stmt->execute();
-                              header("Location: home.php");
+                              header("Location: " . $_SERVER['HTTP_REFERER']);
+                              exit();
 
                             }
                             ?>
