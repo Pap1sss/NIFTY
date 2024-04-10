dbe508d2764c6ba96fcae8683cc9a790510acf0b
Pap1sss <161422556+Pap1sss@users.noreply.github.com>
2024-04-10 19:24:21 +0800

    Update index.php

--- admin/dashboard/index.php
+++ admin/dashboard/index.php
@@ -253,8 +253,7 @@ if ($result->num_rows > 0) {
                             <!-- Page Heading -->
                             <div class="d-sm-flex align-items-center justify-content-between mb-4">
                                 <h1 class="h3 mb-0 text-gray-800">Order Count</h1>
-                                <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-                                        class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+                              
                             </div>
 
                             <!-- Content Row -->
@@ -423,8 +422,7 @@ if ($result->num_rows > 0) {
                             <!-- Page Heading -->
                             <div class="d-sm-flex align-items-center justify-content-between mb-4">
                                 <h1 class="h3 mb-0 text-gray-800">Customer Insights</h1>
-                                <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-                                        class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+                               
                             </div>
                             <div class="row">
 
